83d88e0b7d418fa5cc2fac8eda0811d379c6644b
Kitiyaparnnn <parn.takhum@gmail.com>
2022-03-17 14:41:54 +0700

    exchageRate-exsample

--- moblie_app/lib/main.dart
+++ moblie_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'MoneyBox.dart';
+// import 'MoneyBox.dart';
+import 'package:http/http.dart' as http;
+import 'package:moblie_app/MoneyBox.dart';
+import 'ExchangeRate.dart';
 
 void main() {
   // print("Hello world");
@@ -27,32 +30,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0;
-
   @override
   void initState() {
     super.initState();
-    print("use init State");
+    // print("use init State");
+    exchageRate();
+  }
+
+  Future<ExchangeRate> exchageRate() async {
+    var url = Uri.parse("https://api.exchangerate-api.com/v4/latest/EUR");
+    var response = await http.get(url);
+    ExchangeRate _dataAPI = exchangeRateFromJson(response.body);
+    return _dataAPI;
   }
 
   @override
   Widget build(BuildContext context) {
-    print("use build State");
+    // print("use build State");
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Modern-CSS v.1"),
-      ),
-      body: Column(
-        children: [Text("Message $number")],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            number++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Modern-CSS v.1"),
+        ),
+        body: FutureBuilder(
+            future: exchageRate(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                var result = snapshot.data;
+
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      MoneyBox("EUR", 1, Colors.green, 50),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      MoneyBox("THB", result.rates["THB"], Colors.orange, 40),
+                      SizedBox(height: 10),
+                      MoneyBox("USD", result.rates["USD"], Colors.orange, 40),
+                    ],
+                  ),
+                );
+              }
+              return LinearProgressIndicator();
+            }));
   }
 }
